a8acda80036356cbfd38afe0f9334c9e2f11042b
Chirkovv Artem <patyomkin2011@mail.ru>
2022-06-13 11:36:16 +0300

    start glee 2

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$ (function () {
+  
+  $('.top-slider').slick({
+    arrows: false,
+    dots: true,
+    // autoplay: true,
+    fade: true
+  })
+
+
+
+  var mixer = mixitup('.week-filter__list');
+  var mixer = mixitup('.new-filter__list');
+
+});
